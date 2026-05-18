940f82b26290352678c01c1940d99b6bc76318b0
Abhinav Sharma <abhibede@gmail.com>
2026-05-18 11:52:13 +0530

    practicing python q

--- 0-practice-python-q/1.py
+++ 0-practice-python-q/1.py
@@ -9,15 +9,37 @@
 
 # 2. take three input from use and find the largest number 
 
-a=int(input('Enter the first number= '))
-b=int(input('Enter the second number= '))
-c=int(input('Enter the third number= '))
+# a=int(input('Enter the first number= '))
+# b=int(input('Enter the second number= '))
+# c=int(input('Enter the third number= '))
 
-if a>=b and a>=c:
-    print('the largest number is',a)
-elif b>=a and b>=c:
-    print('the largest number is',b)
-elif c>=a and c>=b:
-    print('the largest number is',c)
+# if a>=b and a>=c:
+#     print('the largest number is',a)
+# elif b>=a and b>=c:
+#     print('the largest number is',b)
+# elif c>=a and c>=b:
+#     print('the largest number is',c)
 
-    
+
+# 3. count vowels in a string
+
+# text= input("enter the string = ")
+# count = 0
+
+# for char in text.lower():
+#     if char in 'aeiou':
+#         count +=1
+# print("no of vowels:",count)        
+
+
+# 4. reverse string without [::-1]
+
+# text = input('Enter the string :')
+# reversed_string=""
+
+# for char in text:
+#     reversed_string=char+reversed_string
+
+# print("reversed string:",reversed_string)
+
+# 5. 
