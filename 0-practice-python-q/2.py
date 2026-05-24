dee9331dc8cdc6833774ce8aadcccc178ba00a7f
Abhinav Sharma <abhibede@gmail.com>
2026-05-24 20:27:58 +0530

    fibonacci series generator

--- 0-practice-python-q/2.py
+++ 0-practice-python-q/2.py
@@ -1,18 +1,33 @@
 # 1 . Check Prime Number
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num <= 1:
-    print("Not a Prime Number")
-else:
-    is_prime = True
+# if num <= 1:
+#     print("Not a Prime Number")
+# else:
+#     is_prime = True
 
-    for i in range(2, num):
-        if num % i == 0:
-            is_prime = False
-            break
+#     for i in range(2, num):
+#         if num % i == 0:
+#             is_prime = False
+#             break
 
-    if is_prime:
-        print(num, "is a Prime Number")
-    else:
-        print(num, "is Not a Prime Number")
+#     if is_prime:
+#         print(num, "is a Prime Number")
+#     else:
+#         print(num, "is Not a Prime Number")
+
+# 2 .Fibonacci Series Generator
+
+n = int(input("Enter number of terms: "))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    
+c = a + b
+a = b
+b = c
+  
