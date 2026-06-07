b4c27acbfe09eb906bbfd1fdd233ddabdc92f9b0
Abhinav Sharma <abhibede@gmail.com>
2026-06-07 16:03:58 +0530

    updating code rank bm

--- 8-Rag/3-rank_bm.py
+++ 8-Rag/3-rank_bm.py
@@ -6,14 +6,14 @@ corpus = [
     "The weather of ludhiana today is very humid"
 ]
 
-tokenized_corpus = [doc.split(" ") for doc in corpus]
+tokenized_corpus = [doc.lower().split(" ") for doc in corpus]
 
 bm25 = BM25Okapi(tokenized_corpus)
 
 
 query = "weather of Ludhiana"
 
-tokenized_query = query.split(" ")
+tokenized_query = query.lower().split(" ")
 
 doc_scores = bm25.get_scores(tokenized_query)
 print(doc_scores)
